44c93e4bac2b3cfb4347ee614f94805ca6cda417
Alex Wiese <wiesee27@outlook.de>
2018-06-22 11:11:07 +0200

    izz daa

--- Spiel.java
+++ Spiel.java
@@ -2,32 +2,29 @@
 /**
  * Beschreiben Sie hier die Klasse Spiel.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author (Alex Wiese, Stepahn Strecker) 
+ * @version (1.0 - 22.06.2018)
  */
 public class Spiel
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
+    private Team heim;
+    private Team aus;
+    
     /**
      * Konstruktor für Objekte der Klasse Spiel
      */
     public Spiel()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    public Team gibWinTeam(){
+        
+        return null;
+    }
+    
+    public Team gibLoseTeam(){
+        return null;
     }
 }
