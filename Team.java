1787fad4ec8a543e6cde7b3bd41b5fe52fc71d93
Stephan Strecker <s-strecker@gmx.net>
2018-06-22 10:50:30 +0200

    neue version

--- Team.java
+++ Team.java
@@ -9,7 +9,7 @@ public class Team
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private String name;
-    private int anzahlSpieler;
+    
 
     /**
      * Konstruktor für Objekte der Klasse Teams
@@ -17,20 +17,11 @@ public class Team
     public Team(String name, int anzahlSpieler)
     {
         this.name = name;
-        this.anzahlSpieler = anzahlSpieler;
     }
 
     public String gibName(){
         return name;
     }
     
-    public int gibAnzahlSpieler(){
-        return anzahlSpieler;
-    }
     
-    public int spielerVerletzt(){
-        int anzahl = 0;
-        return anzahl;
-        //return gibanzahlSpieler() + anzahl;
-    }
 }
