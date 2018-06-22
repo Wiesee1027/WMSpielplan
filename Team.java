22dfd5b962fec92747a7868926abc72bd5758250
Alex Wiese <wiesee27@outlook.de>
2018-06-22 10:15:31 +0200

    Gemeinsames Projekt angelegt

---
+++ Team.java
@@ -0,0 +1,35 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Teams.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Team
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private String name;
+    private int anzahlSpieler;
+
+    /**
+     * Konstruktor für Objekte der Klasse Teams
+     */
+    public Team(String name, int anzahlSpieler)
+    {
+        this.name = name;
+        this.anzahlSpieler = anzahlSpieler;
+    }
+
+    public String gibName(){
+        return name;
+    }
+    
+    public int gibanzahlSpieler(){
+        return anzahlSpieler;
+    }
+    
+    public int spielerVerletzt(){
+        int anzahl = 0;
+        return gibanzahlSpieler() + anzahl;
+    }
+}
