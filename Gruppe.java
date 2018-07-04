a0b2bfaa6254a104c7ca213b1a3482a23e875066
Stephan Strecker <s-strecker@gmx.net>
2018-07-04 11:53:40 +0200

    jo

--- Gruppe.java
+++ Gruppe.java
@@ -29,26 +29,32 @@ public class Gruppe
      */
     public void spieleErstellen()
     {
-       ArrayList<Team>m=new ArrayList<Team>(mannschaften);
+
         gruppenSpiele=new ArrayList<Spiel>();
         Random random = new Random();
-
-        while(!m.isEmpty())
+        int anzahlspiele=mannschaften.size()*2;
+        while(gruppenSpiele.size()<anzahlspiele)
         {
-            Team spieler2 = null;
-            int zahl = random.nextInt(m.size());
-            Team  spieler1 = m.get(zahl);  
-            m.remove(zahl);
-            zahl = random.nextInt(m.size());
-            spieler2 = m.get(zahl);  
-            m.remove(zahl);
- 
-
-
-            gruppenSpiele.add(new Spiel(spieler1, spieler2));
-        } 
+            ArrayList<Team>m=new ArrayList<Team>(mannschaften);
+            while(!m.isEmpty())
+            {
+                Team spieler2 = null;
+                int zahl = random.nextInt(m.size());
+                Team  spieler1 = m.get(zahl);  
+                m.remove(zahl);
+                zahl = random.nextInt(m.size());
+                spieler2 = m.get(zahl);  
+                m.remove(zahl);
+                for(Spiel spiel:gruppenSpiele)
+                {
+                    if(spiel.gibHeimTeam()!=spieler1 && spiel.gibGastTeam()!=spieler2 && spiel.gibGastTeam()!= spieler1 && spiel.gibHeimTeam() != spieler2)
+                    {
+                        gruppenSpiele.add(new Spiel(spieler1, spieler2));
+                    } 
+                }
+            }
+        }
     }
-
 
     public void ordne()
     {
@@ -102,17 +108,16 @@ public class Gruppe
         }
         return punkte;
     }
-    
-    
+
     public void ordnen()
     {
         for(Team team:mannschaften)
         {
-         if(gibGesamtPunkte(team)<gibGesamtPunkte(mannschaften.get(mannschaften.indexOf(team)+1)))
-         {
-             
-         }
-         
+            if(gibGesamtPunkte(team)<gibGesamtPunkte(mannschaften.get(mannschaften.indexOf(team)+1)))
+            {
+
+            }
+
         }
     }
 }
